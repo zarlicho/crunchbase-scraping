ce2beb2cedef1d520e78f7982a53a8f13a2fbf05
zarlicho <fatihhurisqi1492005@gmail.com>
2024-11-14 20:35:22 +0700

    update for airtable fuzzy match

--- test/airtables_test.py
+++ test/airtables_test.py
@@ -1,4 +1,4 @@
-import time,json,requests,os,urllib.parse
+import json,requests,os,urllib.parse
 from dotenv import load_dotenv
 from thefuzz import fuzz
 import pandas as pd
@@ -7,9 +7,9 @@ load_dotenv()
 class Airtable:
     def __init__(self):
         self.apikey = os.getenv('AIRTABLE_apikey')
-        self.headers = {'Authorization': f'Bearer {self.apikey}'}
-        self.PostHead = {'Authorization': f'Bearer {self.apikey}','Content-Type': 'application/json'}
-    
+        self.headers = {'Authorization': f'Bearer {self.apikey}','Content-Type': 'application/json'}
+        self.GetInvestors()
+
     def Credd(self,view,crmid,prostable):
         return [view,crmid,prostable]
 
@@ -20,16 +20,14 @@ class Airtable:
         offset = ''
         while True:
             CompanyTableURL = f'https://api.airtable.com/v0/{cred[1]}/{cred[2]}'
-            params = {'offset': offset}
+            params = {'offset': offset,"view":''}
             if cred[0]:
                 params['view'] = cred[0]
             OutputTable = requests.get(CompanyTableURL, headers=self.headers, params=params).json()
-            
             for record in OutputTable["records"]:
                 fields = record.get("fields", {})
                 if "Company Name" not in fields:
                     continue
-                
                 SingleRecord = {
                     "Company": fields["Company Name"],
                     "HQ": fields.get('HQ', ''),
@@ -45,6 +43,35 @@ class Airtable:
         if AllRecordIds:
             print("Done!")
             return AllRecordIds
+
+    def GetInvestors(self):
+        print("Get all Investors data...")
+        AllRecordIds = []
+        offset = ''
+        while True:
+            CompanyTableURL = f'https://api.airtable.com/v0/appjvhsxUUz6o0dzo/tblOws03Uc7SOVAKI'
+            params = {'offset': offset,"view":''}
+            params['view'] = "Grid view"
+            OutputTable = requests.get(CompanyTableURL, headers=self.headers, params=params).json()
+            for record in OutputTable["records"]:
+                fields = record.get("fields", {})
+                if "Name" not in fields:
+                    continue
+                SingleRecord = {"InvName": fields["Name"],"RecordId":record['id']}
+                print("Investor Name: ", SingleRecord["InvName"])
+                AllRecordIds.append(SingleRecord)
+            if "offset" not in OutputTable:
+                break
+            offset = OutputTable["offset"]
+        if AllRecordIds:
+            with open('InvestorData.json','w+') as Data:
+                Data.write(json.dumps(AllRecordIds))
+
+    def FindInvestor(self,invName):
+        with open('./InvestorData.json','r+') as inv:
+            investors = json.load(inv)
+        investor = next((i for i in investors if i['InvName'] == invName), None)
+        return investor['RecordId'] if investor else None, print(f'Investor "{invName}" not found!')
 
     def FuzMatch(self,CompanyRecords):
         print("Checking Company...")
@@ -71,32 +98,42 @@ class Airtable:
                     }
                 })
                 try:
-                    ProsRec = self.CreateField(cred=ar.Credd(view="ATX Ventures",crmid="appjvhsxUUz6o0dzo",prostable="tblf4Ed9PaDo76QHH"),data=ProsData)
-                    CompRec = self.CreateField(cred=ar.Credd(view="ATX Ventures",crmid="appjvhsxUUz6o0dzo",prostable="tblEssTJ1FlLrTj8Q"),data=CompData)
-                    self.UpRec(cred=ar.Credd(view="ATX Ventures",crmid="appjvhsxUUz6o0dzo",prostable="tblf4Ed9PaDo76QHH"),data={"fields":{"Company Name":df.iloc[row_index,1],"Link to Company":[CompRec]}})
-                    self.UpRec(cred=ar.Credd(view="ATX Ventures",crmid="appjvhsxUUz6o0dzo",prostable="tblEssTJ1FlLrTj8Q"),data={"fields":{"Company Name":df.iloc[row_index,1],"Is Prospect (Link to Prospects)":[ProsRec]}})
+                    CompRec = self.CreateField(cred=ar.Credd(view="Company",crmid="appjvhsxUUz6o0dzo",prostable="tblEssTJ1FlLrTj8Q"),data=CompData)
+                    ProsRec = self.CreateField(cred=ar.Credd(view=df.iloc[row_index,0],crmid="appjvhsxUUz6o0dzo",prostable="tblf4Ed9PaDo76QHH"),data=ProsData)
+                    self.UpRec(cred=ar.Credd(view=df.iloc[row_index,0],crmid="appjvhsxUUz6o0dzo",prostable="tblf4Ed9PaDo76QHH"),data={"fields":{"Company Name":df.iloc[row_index,1],"Link to Company":[CompRec],"Linked Investors":[self.FindInvestor(invName=df.iloc[row_index,0])],"Investors":[self.FindInvestor(invName=df.iloc[row_index,0])]}},recId=ProsRec)
+                    self.UpRec(cred=ar.Credd(view="Company",crmid="appjvhsxUUz6o0dzo",prostable="tblEssTJ1FlLrTj8Q"),data={"fields":{"Company Name":df.iloc[row_index,1],"Is Prospect (Link to Prospects)":[ProsRec],"Linked Investors (Link to Investors)":[self.FindInvestor(invName=df.iloc[row_index,0])]}},recId=CompRec)
                 except ValueError:
                     print("something error when creating data")
 
-    def UpRec(self,cred,data):
+    def DeleteFields(self,recId,tabId):
+        response = requests.delete(f"https://api.airtable.com/v0/appjvhsxUUz6o0dzo/{tabId}/{recId}",headers=self.headers)
+        if response.status_code !=200:
+            raise ValueError
+        else:
+            return f'success to deleting {recId} data'
+
+    def UpRec(self,cred,data, recId):
         companyName = data['fields']['Company Name']
         response = requests.request(
             "PATCH",
             f"https://api.airtable.com/v0/{cred[1]}/{cred[2]}/"
             f"{requests.request('GET', f'https://api.airtable.com/v0/{cred[1]}/{cred[2]}?filterByFormula=%7BCompany+Name%7D%3D%27{urllib.parse.quote_plus(companyName)}%27', headers=self.headers).json()['records'][0]['id']}",
-            headers=self.PostHead,
+            headers=self.headers,
             data=json.dumps(data)
         )
-
         print("CRM PATHCING STATUS: ", response.status_code)
         if response.status_code != 200:
             print(f"something error when updating data to airtable. {response.status_code}")
-            raise ValueError
+            try:
+                print(self.DeleteFields(recId=recId,tabId=cred[2]))
+            except ValueError:
+                print(f"failed to delete {list(data['fields'].values())[1][0]} field!")
+            # raise ValueError
         else:
             print(f"UPDATING {companyName} TO CRM DONE!")
 
     def CreateField(self,cred,data):
-        response = requests.request("POST", f'https://api.airtable.com/v0/{cred[1]}/{cred[2]}', headers=self.PostHead, data=data)
+        response = requests.request("POST", f'https://api.airtable.com/v0/{cred[1]}/{cred[2]}', headers=self.headers, data=data)
         if response.status_code !=200:
             raise ValueError
         else:
@@ -105,5 +142,7 @@ class Airtable:
 
 if __name__ == "__main__":
     ar = Airtable()
-    credential = ar.Credd(view="ATX Ventures",crmid="appjvhsxUUz6o0dzo",prostable="tblf4Ed9PaDo76QHH")
+    credential = ar.Credd(view="Nashville Entrepreneur Center",crmid="appjvhsxUUz6o0dzo",prostable="tblf4Ed9PaDo76QHH")
     ar.FuzMatch(CompanyRecords=ar.GetCompany(cred=credential))
+    # ar.GetInvestors()
+    # ar.DeleteFields(recId="rec02wx281njjRjhm",tabId="tblf4Ed9PaDo76QHH")
